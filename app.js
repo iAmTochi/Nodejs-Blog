6cc83c8b1fe8d358207ccd6afce7381b2ae3ce97
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-03-24 17:07:14 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const hbs = require('express-handlebars');
+const {mongoDbUrl} = require('./config/configuration')
+
+
+
+const app = express();
+
+
+
+
+//Configure Mongoose to Connect to MongoDB
+mongoose.connect(mongoDbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(response => {
+        console.log("MongoDB Connected Successfully");
+    }).catch( err => {
+        console.log("Database Connection Failed.");
+})
+
+
+//Configure express
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Setup view Engine To Use Handlebars
+app.engine('handlebars', hbs({defaultLayout: 'default'}));
+app.set('view engine', 'handlebars');
+
+
+
+
+// Routes
+const defaultRoutes = require('./routes/defaultRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+app.use('/', defaultRoutes);
+app.use('/admin', adminRoutes);
+
+
+app.listen(3000, () =>{
+    console.log(`Server is running on port 3000`);
+});
---
+++ config/configuration.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoDbUrl : 'mongodb://localhost:27017/nodejs_cms',
+    PORT : process.env.PORT || 3000,
+}
